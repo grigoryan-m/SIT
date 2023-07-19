9b8146f9d2af5519518c8daff96ab77e1ec2360d
grigoryan-m <waldm4rk@gmail.com>
2023-07-19 15:44:42 +0400

    Hamburger menu now working on small screens. Everything is responsive now.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
-window.onload = function(){
-	const input = document.getElementById("search_bar");
-	let objects = document.getElementsByClassName("object");
-
-	input.addEventListener("input", log);
-
-	function log(e){
-		for(let i = 0; i < objects.length; i++){
-			if(!(objects[i].innerText.toLowerCase().includes(e.target.value.toLowerCase()))){
-				objects[i].style.display = "none";
-			}else{
-				objects[i].style.display = "block";
-			}
-		}	
+function hamburger(){
+	var menu = document.getElementsByClassName("hamburger")[0];
+	if(!menu.classList.contains("active")){
+		menu.classList.add("active");
+	}else{
+		menu.classList.remove("active");
 	}
-}
+}	
